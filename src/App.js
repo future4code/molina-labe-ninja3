62ed1bba8edd87c801df18b00cb3f8e0729ee1de
suphessel <suhessel28@gmail.com>
2021-06-22 15:24:50 -0300

    Arrumei os componentes home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import React from "react";
-import { AppContainer } from "./componentsBruno/AppContainer";
+import AppContainer from "./ComponentesHome/AppContainer";
 import CardAulas from "./componentsBruno/CardAulas/CardAulas";
+import CardsCategorias from "./ComponentesHome/CardsCategorias";
+import styled from 'styled-components'
 
-function App() {
-  return <CardAulas />;
+const AppContainerHome = styled.div `
+	
+	height: 100rem;
+	
+
+ `
+
+export default class App extends React.Component {
+
+
+	render () {
+		return (
+			<AppContainerHome>
+
+			<AppContainer/>
+
+			<CardAulas />
+			<CardsCategorias/>
+
+			</AppContainerHome>
+		) 
+	}
+	
+  
+
 }
 
-export default App;
+
