420bd81b2081bb8741c6774df14e1171b5882242
edersonamaral <ederson.rafael30@gmail.com>
2021-06-22 16:16:56 -0300

    criacaoDoComponentAuto

--- src/App.js
+++ src/App.js
@@ -1,29 +1,23 @@
 import React from "react";
-import AppContainer from "./ComponentesHome/AppContainer";
 import CardAulas from "./componentsBruno/CardAulas/CardAulas";
+import CardAutos from "./ComponentsAutos/CardAutos";
 import CardsCategorias from "./ComponentesHome/CardsCategorias";
 import styled from 'styled-components'
-
-const AppContainerHome = styled.div `
-	
-	height: 100rem;
-	
-
- `
 
 export default class App extends React.Component {
 
 
 	render () {
 		return (
-			<AppContainerHome>
-
-			<AppContainer/>
+			<>
 
 			<CardAulas />
+			<CardAutos />
 			<CardsCategorias/>
 
-			</AppContainerHome>
+			</>
+
+			
 		) 
 	}
 	
